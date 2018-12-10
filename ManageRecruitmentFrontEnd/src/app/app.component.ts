7ffca3c5775fe54a80027fc7c6df08953610740f
Masoom Ul Haq S <masoomulhaqs@gmail.com>
2018-12-10 15:00:59 +0530

    login page: hardcoded hiding header for demo

--- ManageRecruitmentFrontEnd/src/app/app.component.ts
+++ ManageRecruitmentFrontEnd/src/app/app.component.ts
@@ -1,8 +1,28 @@
 ﻿import { Component } from '@angular/core';
+import { Router, ActivatedRoute, NavigationStart } from '@angular/router';
 
 @Component({
     selector: 'app',
     templateUrl: 'app.component.html'
 })
 
-export class AppComponent { }
+export class AppComponent { 
+    showHead: any;
+
+    constructor(
+        private router: Router,
+        public aroute: ActivatedRoute
+      ) { 
+            router.events.forEach((event) => {
+                console.log(event);
+            if (event instanceof NavigationStart) {
+              if (event['url'] == '/login' || event['url'] == '/login?returnUrl=%2F') {
+                this.showHead = false;
+              } else {
+                // console.log("NU")
+                this.showHead = true;
+              }
+            }
+          });
+      }
+}
