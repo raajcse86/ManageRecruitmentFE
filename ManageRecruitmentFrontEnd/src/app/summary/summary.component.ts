8a43589ffe47fd4d7e058ca735ef6bd3494bf041
Palli268 <46524253+Palli268@users.noreply.github.com>
2019-01-24 13:16:52 +0530

    Changes made for Dynamic Data In Summary Table

--- ManageRecruitmentFrontEnd/src/app/summary/summary.component.ts
+++ ManageRecruitmentFrontEnd/src/app/summary/summary.component.ts
@@ -4,6 +4,7 @@ import { CandidatureDetailsService } from './../_services/candidature-detai
 import { CandidatureDetails } from '../_models';
 import { first } from 'rxjs/operators';
 import { chart } from '../_models/chart';
+import { Summary } from '../_models/summary';
 
 @Component({
 selector: 'app-summary',
@@ -12,16 +13,17 @@ styleUrls: ['./summary.component.css']
 })
 export class SummaryComponent implements OnInit {
 candidatures: CandidatureDetails[] = [];
+summarylist:Summary[];
 public pie_chartType: string = 'bar';
 chartRawData: chart;
-showTable:boolean;
+
 public pie_chartLabels: Array<String> = [];
 public pie_chartDatasets: Array<any> = [];
 constructor( private candidatureDetailsService: CandidatureDetailsService) { 
 }
 
 ngOnInit() {
-this.candidatureDetailsService.getCandidaturesChart('client').pipe(first()).subscribe(candidatureFromService => { 
+/*this.candidatureDetailsService.getCandidaturesChart('client').pipe(first()).subscribe(candidatureFromService => { 
 this.chartRawData = candidatureFromService; 
 console.log('chartRawData ::' +this.chartRawData.chartDatasets[1].data);
 this.pie_chartLabels=this.chartRawData.chartLabels;
@@ -29,8 +31,23 @@ this.pie_chartDatasets=this.chartRawData.chartDatasets;
 if(this.pie_chartLabels.length>0){
 this.showTable=true;
 }
-});
+});*/
+
+this.candidatureDetailsService.getClientSummary().subscribe(
+    data => {
+        console.log(data);
+        this.summarylist=data;
+        
+     },
+    error  => {
+    console.log("Error", error);
+    });
+
+
 }
+
+
+
 public pie_chartClicked(e: any): void {
 console.log(e);
 }
