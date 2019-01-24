ea2e99446cbbfaf57b982b825f224f90a7324003
Palli268 <46524253+Palli268@users.noreply.github.com>
2019-01-24 13:12:53 +0530

    Add files via upload
    Summary Client Dynamic data

---
+++ ManageRecruitmentFrontEnd/src/app/_models/summary.ts
@@ -0,0 +1,15 @@
+
+export class Summary {
+
+    clientName: string;
+    leadName: string;
+    location: string;
+    skill: string;
+    contractMechanism: string;
+    target: string;
+    interviewInProgress: number;
+    joined: number;
+    offerInProgress: number;
+    screeningInProgress: number;
+    offerReleased: number; 
+}
