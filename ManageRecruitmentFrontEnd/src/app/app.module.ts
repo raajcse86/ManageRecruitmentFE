5a41afc515b0fc429208c04490ee6ca22a86c19f
Soni Kumari <Soni2.Kumari@in.ey.com>
2019-02-05 16:43:20 +0530

    Soni: Removed fake backend

--- ManageRecruitmentFrontEnd/src/app/app.module.ts
+++ ManageRecruitmentFrontEnd/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule,ReactiveFormsModule }    from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 // used to create fake backend
-import { fakeBackendProvider } from './_helpers';
 import { AppComponent }  from './app.component';
 import { routing }        from './app.routing';
 import { AlertComponent } from './_directives';
@@ -89,9 +88,6 @@ import {DropdownModule} from 'primeng/dropdown';
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorJwtAuthService, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-
-        // provider used to create fake backend
-        fakeBackendProvider
     ],
     bootstrap: [AppComponent]
 })
