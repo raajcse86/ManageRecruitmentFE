922a89a50f3e94ee269f7154cc8f5f8e03cb94e7
nikhil-madhamanchi <46524214+nikhil-madhamanchi@users.noreply.github.com>
2019-02-13 10:36:56 +0530

    Changes made for login page validation

--- ManageRecruitmentFrontEnd/src/app/_helpers/error.interceptor.ts
+++ ManageRecruitmentFrontEnd/src/app/_helpers/error.interceptor.ts
@@ -5,21 +5,27 @@ import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor() {}
+  constructor() { }
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(catchError((error: HttpErrorResponse) => {
-            let errorMessage = '';
-         if (error.error instanceof ErrorEvent) {
-           // client-side error
-           errorMessage = `Error: ${error.error.message}`;
-         } else {
-           // server-side error
-           errorMessage = `Username or password is incorrect`;
-         }
-        // window.alert(errorMessage);
-         return throwError(errorMessage);
-       
-        }))
-    }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(catchError((error: HttpErrorResponse) => {
+      let errorMessage = '';
+      if (error.error instanceof ErrorEvent) {
+        // client-side error
+        errorMessage = `Error: ${error.error.message}`;
+      } else {
+        // server-side error
+        if (error.statusText == 'Unknown Error') {
+          errorMessage = 'Unable to connect Server'
+
+        } else {
+          errorMessage = `Error: ${error.error.message}`;
+        }
+
+
+      }
+      return throwError(errorMessage);
+
+    }))
+  }
 }
